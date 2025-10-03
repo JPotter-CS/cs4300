bfadb1ea61dfd6830c86d65076840331bf4643f3
Jake Potter <pakejotter02@gmail.com>
2025-10-03 21:28:12 +0000

    Added Database configuration to Settings.py

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -54,4 +54,12 @@ TEMPLATES = [
     },
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 STATIC_URL = '/static/'
